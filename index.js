e1c757897d198c620024ccd37da5703cf0a62d5f
Shubham Patil <shubhampatilsd@gmail.com>
2022-06-13 15:44:53 -0700

    express

--- index.js
+++ index.js
@@ -1,56 +1,24 @@
-const puppeteer = require("puppeteer");
-const fs = require("fs");
-const nodemailer = require("nodemailer");
-require("dotenv").config();
+const express = require("express");
+const { bot } = require("./bot");
+require("dotenv").config()
 
-(async () => {
-  const browser = await puppeteer.launch({headless: true, args:["--no-sandbox"]});
-  const page = await browser.newPage();
-  await page.goto(
-    "https://www.xbox.com/en-US/games/store/hitman-3---standard-edition/9PF0R4MP0MW7"
-  );
+const app = express();
 
-  const element = await page.$(".Price-module__boldText___34T2w");
+app.get("/", (req, res) => {
+  res.send("why hello there");
+});
 
-  //   console.log(element);
+app.get("/get", (req,res)=>{
+    await bot();
+    res.send("Done")
+})
 
-  const value = await element.evaluate((el) => el.textContent);
-  if (
-    parseFloat(value.substring(1, value.length - 1)) * 100 <
-    parseFloat(fs.readFileSync("./yesterday.txt").toString()) * 100
-  ) {
-    console.log("On sale");
-    let testAccount = await nodemailer.createTestAccount();
+const PORT = process.env.PORT || 5000
 
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL, // generated ethereal user
-        pass: process.env.PASSWORD, // generated ethereal password
-      },
-    });
+app.listen(PORT, (err)=>{
+    if(err) throw err;
+    console.log(`Listening on port ${PORT}`)
 
-    // send mail with defined transport object
-    let info = await transporter.sendMail({
-      from: "shubhampatilsd@gmail.com", // sender address
-      to: "shubhampatilsd@gmail.com", // list of receivers
-      subject: "Hitman 3 on sale", // Subject line
-      text: "Hitman 3 is now on sale for Xbox at " + value, // plain text body
-    });
+})
 
-    console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-    // Preview only available when sending through an Ethereal account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  }
-
-  try {
-    fs.writeFileSync("./yesterday.txt", value.substring(1, value.length - 1));
-  } catch (err) {
-    console.log(err);
-  }
-
-  await browser.close();
-})();
